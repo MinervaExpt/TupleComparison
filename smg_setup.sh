2ab38d1aff1fc980eb7b2502c42e855a891c142e
smgilligan <gilligas@oregonstate.edu>
2022-07-05 16:22:23 -0500

    smg_setup

--- smg_setup.sh
+++ smg_setup.sh
@@ -2,3 +2,5 @@ export VALIDATIONTOOLSROOT=$WHEREIPUTMYCODE/TupleComparison
 export TUPLECOMPARISONROOT=$VALIDATIONTOOLSROOT
 export HIST1=/minerva/app/users/gilligan/minerva/CCQENu/make_hists/smg/validation/SB_AntiNu_v9_CCQENu_1.root
 export HIST2=/minerva/app/users/gilligan/minerva/CCQENu/make_hists/smg/validation/SB_AntiNu_v9_MAD_1.root
+#export HIST1=/minerva/app/users/gilligan/minerva/CCQENu/make_hists/smg/validation/SB_Nu_v9_CCQENu_1.root
+#export HIST2=/minerva/app/users/gilligan/minerva/CCQENu/make_hists/smg/validation/SB_Nu_v9_MAD_1.root
